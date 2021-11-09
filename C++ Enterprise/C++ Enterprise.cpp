c3af57fc688e611f09ffb8f02ed65d8a4961c46f
Sanches Mateus Alexandre <alexandre.sanches_mateus@edu.devinci.fr>
2021-11-09 11:02:13 +0100

    Debug + ajustement des fonctionalités de base (switch pour event)

--- C++ Enterprise/C++ Enterprise.cpp
+++ C++ Enterprise/C++ Enterprise.cpp
@@ -1,24 +1,39 @@
+
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "C++ Enterprise");
-    window.setVerticalSyncEnabled(true);
+    window.setVerticalSyncEnabled(true);  // Frame rate de l'écran
 
     sf::Clock clock;
-    
+
     while (window.isOpen()) {
+
+        // Input
         sf::Event event;
-        while (window.pollEvent(event)) {{
-            // Process any input event here
-            if (event.type == sf::Event::Closed) {
-                window.close();
+        while (window.pollEvent(event)) {
+            {
+                switch(event.type)
+                {
+				case sf::Event::Closed:
+                    window.close();
+                    break;
+
+                default:
+                    break;
+                }
             }
+
+            // Logique
+            sf::Time elapsedTime = clock.restart();
+
+            // Rendu
+            window.clear();
+
+            // Whatever I want to draw goes here
+            window.display();
         }
-        sf::Time elapsedTime = clock.restart();
-        window.clear();
-        // Whatever I want to draw goes here
-        window.display();
     }
 }
