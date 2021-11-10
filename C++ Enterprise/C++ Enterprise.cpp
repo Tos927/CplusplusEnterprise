b8324c1f92a7c55d435616cc0595a4fa341ac40a
Tos927 <t.sautereau92@gmail.com>
2021-11-10 13:50:32 +0100

    Ajout du canon

--- C++ Enterprise/C++ Enterprise.cpp
+++ C++ Enterprise/C++ Enterprise.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-void ShipMovement(sf::CircleShape& ship, float deltaTime);
+void ShipMovement(sf::CircleShape& ship, sf::RectangleShape& weapon, float deltaTime);
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "C++ Enterprise");
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "C++ Enterprise");
     window.setVerticalSyncEnabled(true);
 
     sf::CircleShape ship(30.0f,30);
+    ship.setOrigin(30.0f, 30.0f);
     ship.setPosition(sf::Vector2f(400.0f, 300.0f));
+
+    sf::RectangleShape weapon(sf::Vector2f(50.0f, 10.0f));
+    weapon.setOrigin(0.0f, 5.0f);
+    weapon.setPosition(sf::Vector2f(400.0f, 300.0f));
 
     sf::Clock clock;
     
@@ -32,29 +37,42 @@ int main()
 
         // Logique
         sf::Time elapsedTime = clock.restart();
-        ShipMovement(ship, elapsedTime.asSeconds());
+        ShipMovement(ship, weapon,  elapsedTime.asSeconds());
 
         // Rendu
         window.clear();
 
         window.draw(ship);
+        window.draw(weapon);
 
         window.display();
     }
 }
 
-void ShipMovement(sf::CircleShape& ship, float deltaTime)
+void ShipMovement(sf::CircleShape& ship, sf::RectangleShape& weapon, float deltaTime)
 {
     float speed = 300.0f;
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-		ship.move(sf::Vector2f(0.f, -speed * deltaTime));
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) 
+    {
+        ship.move(sf::Vector2f(0.f, -speed * deltaTime));
+        weapon.move(sf::Vector2f(0.f, -speed * deltaTime));
+    }
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+    {
 		ship.move(sf::Vector2f(0.f, speed * deltaTime));
+        weapon.move(sf::Vector2f(0.f, speed * deltaTime));
+    }
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+    {
 		ship.move(sf::Vector2f(-speed * deltaTime, 0.f));
+        weapon.move(sf::Vector2f(-speed * deltaTime, 0.f));
+    }
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+    {
 		ship.move(sf::Vector2f(speed * deltaTime, 0.f));
+        weapon.move(sf::Vector2f(speed * deltaTime, 0.f));
+    }
 }
